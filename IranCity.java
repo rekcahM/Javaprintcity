6873a050e99ffeaefa40fc4435a7522440f25552
Manihedayati <142336467+rekcahM@users.noreply.github.com>
2025-04-28 15:40:55 +0330

    Add files via upload

---
+++ IranCity.java
@@ -0,0 +1,9 @@
+
+public class IranCity {
+    public static void main(String[] args) {
+        System.out.println("Tehran");
+        System.out.println("Isfahan");
+        System.out.println("Shiraz");
+        System.out.println("Tabriz");
+    }
+}
